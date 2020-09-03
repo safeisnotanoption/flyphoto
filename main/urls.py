322484e55908f42e87aa3f4f7db0af8d34a1dbdc
ID <safeisnotanoption@gmail.com>
2020-09-03 05:10:53 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('edit_project', views.edit_project, name='edit_project'),
     path('contacts', views.contacts, name='contacts'),
-    path('photo_list', views.photo_list, name='photo_list'),
 ]
